ad451805e9386134c7310f7135577a6d286d6923
Ioannis Loukeris <john.loukeris@basculeuw.com>
2023-07-18 17:10:39 +0200

    * added forgotten error constant

--- src/Responses/ErrorType.php
+++ src/Responses/ErrorType.php
@@ -9,4 +9,5 @@ final class ErrorType extends Enum
   const SERVER_ERROR = '00000';
   const CREATE_VALIDATION_FAILED = '00001';
   const UPDATE_VALIDATION_FAILED = '00002';
+  const RESOURCE_NOT_FOUND = '00003';
 }
